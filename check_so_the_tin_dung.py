ff4ebcbb2dc1fcc4cd8f937bff5dee26ae4dd993
dungbi3011 <67071625+dungbi3011@users.noreply.github.com>
2020-08-16 22:47:39 +0700

    Update check_so_the_tin_dung.py

--- check_so_the_tin_dung.py
+++ check_so_the_tin_dung.py
@@ -1,5 +1,5 @@
-def check_chan_le(so_bat_ki):
-    if so_bat_ki %2 == 0:
+def check_even_odd(so_bat_ki):
+    if so_bat_ki %2 != 0:
         return True
     else:
         return False
@@ -12,20 +12,22 @@ def double_and_check_greater_than_nine(so_bat_ki):
         return so_bat_ki * 2
 
 def valid_credit_card_number():
-    print("So the tin dung ban vua nhap vao kha dung")
+    print("VALID CREDIT CARD NUMBER")
 
 def invalid_credit_card_number():
-    print("So the tin dung ban vua nhap vao khong kha dung")
+    print("INVALID CREDIT CARD NUMBER")
 
+
+#where this program begins
 def main():
     main_check = True
     while main_check:
-        credit_card_number = reversed(list(input("Nhap so the tin dung ma ban can check vao day")))
+        credit_card_number = reversed(list(input("Type the credit card number that you want to check: ")))
         for chu_so in credit_card_number:
             if chu_so.isdigit == True:
                 pass
             else:
-                invalid_credit_card_number()
+                invalid_credit_card_number() # invalid credit card number
                 main_check = False
 
     # day so moi cua the tin dung
@@ -35,13 +37,13 @@ def main():
         if 12 <= so_chu_so <= 19:
             pass
         else:
-            invalid_credit_card_number()
+            invalid_credit_card_number()     # invalid creadit card number
             break
 
     #check de them nhung chu so tu thu 1 cho den cuoi
         for x in range (0, len(credit_card_number) - 1) :
-           chan_hay_le = check_chan_le(x)
-           if chan_hay_le == True:
+           even_or_odd = check_even_odd(x)
+           if even_or_odd == True:
                y = double_and_check_greater_than_nine(credit_card_number[x])
                day_so_moi.append(y)
            else:
@@ -59,12 +61,3 @@ def main():
         else:
             invalid_credit_card_number()
             break
-
-
-
-
-
-
-
-
-
